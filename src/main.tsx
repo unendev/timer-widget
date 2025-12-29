51617b24d199405726a696e07320e1a58e09aec6
helpfulcraft <a1634358912@outlook.com>
2025-12-29 23:04:46 +0800

    fix: set correct NextAuth basePath for Electron production

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { SessionProvider } from 'next-auth/react';
 import App from './App';
 import './index.css';
+import { API_BASE_URL } from './lib/api';
+
+const basePath = API_BASE_URL ? `${API_BASE_URL}/api/auth` : '/api/auth';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <SessionProvider>
+    <SessionProvider basePath={basePath}>
       <App />
     </SessionProvider>
   </React.StrictMode>
