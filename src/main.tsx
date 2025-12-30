b6d86be86ad57fc2826b49e79ea664f808b371c9
helpfulcraft <a1634358912@outlook.com>
2025-12-30 11:12:28 +0800

    fix: remove SessionProvider to resolve sendBeacon error and decouple from next-auth client

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { SessionProvider } from 'next-auth/react';
 import App from './App';
 import './index.css';
-import { API_BASE_URL } from './lib/api';
-
-const basePath = API_BASE_URL ? `${API_BASE_URL}/api/auth` : '/api/auth';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <SessionProvider basePath={basePath}>
-      <App />
-    </SessionProvider>
+    <App />
   </React.StrictMode>
 );
